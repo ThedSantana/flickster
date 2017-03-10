7bb31ff7563ca8dc72f6c59dd0e8da5480839285
Lukas Blakk <lukasblakk@gmail.com>
2017-03-10 15:49:24 -0800

    Move the youTubeKey logic to when movies is being populated

--- app/src/main/java/com/lukasblakk/flixster/activities/MovieActivity.java
+++ app/src/main/java/com/lukasblakk/flixster/activities/MovieActivity.java
@@ -1,9 +1,8 @@
 package com.lukasblakk.flixster.activities;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -44,7 +43,7 @@ public class MovieActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // Grab the movie that is tapped on in the view
                 Movie movie = movieAdapter.getItem(position);
-                launchYouTubeView(movie);
+                launchYouTubeView(movie.getYouTubeKey());
             }
         });
 
@@ -59,7 +58,36 @@ public class MovieActivity extends AppCompatActivity {
                 try {
                     movieJsonResults = response.getJSONArray("results");
                     movies.addAll(Movie.fromJSONArray(movieJsonResults));
-                    movieAdapter.notifyDataSetChanged();
+
+                    // Fill the movie youTubeKey params
+                    for (int x = 0; x < movies.size(); x++) {
+                        if (movies.get(x).getYouTubeKey() == "") {
+                            String videosUrl = String.format("https://api.themoviedb.org/3/movie/%s/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US", movies.get(x).getId());
+                            final int position = x;
+                            AsyncHttpClient videoClient = new AsyncHttpClient();
+
+                            videoClient.get(videosUrl, new JsonHttpResponseHandler() {
+
+                                @Override
+                                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                                    JSONArray videoJsonResults = null;
+
+                                    try {
+                                        videoJsonResults = response.getJSONArray("results");
+                                        movies.get(position).setYouTubeKey(videoJsonResults.getJSONObject(0).getString("key"));
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+
+                                @Override
+                                public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                                    super.onFailure(statusCode, headers, throwable, errorResponse);
+                                }
+                            });
+                        }
+                        movieAdapter.notifyDataSetChanged();
+                    }
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -70,45 +98,17 @@ public class MovieActivity extends AppCompatActivity {
                 super.onFailure(statusCode, headers, throwable, errorResponse);
             }
         });
+
+
     }
 
-    public void launchYouTubeView(Movie movie) {
-        String youTubeKey = fetchYouTubeKey(movie);
-
-        // first parameter is the context, second is the class of the activity to launch
-        Intent i = new Intent(MovieActivity.this, YouTubePlayerActivity.class);
-        // put youTube key into the bundle for access in the youTube activity
-        i.putExtra("youTubeKey", youTubeKey);
-        startActivity(i); // brings up the second activity
-    }
-
-    public String fetchYouTubeKey(final Movie movie){
-
-        if (movie.getYouTubeKey() == "") {
-            String videosUrl = String.format("https://api.themoviedb.org/3/movie/%s/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US", movie.getId());
-
-            AsyncHttpClient client = new AsyncHttpClient();
-
-            client.get(videosUrl, new JsonHttpResponseHandler() {
-                @Override
-                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                    JSONArray videoJsonResults = null;
-
-                    try {
-                        videoJsonResults = response.getJSONArray("results");
-                        movie.setYouTubeKey(videoJsonResults.getJSONObject(0).getString("key"));
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                @Override
-                public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                    super.onFailure(statusCode, headers, throwable, errorResponse);
-                }
-            });
+    public void launchYouTubeView(String youTubeKey) {
+        if (!youTubeKey.isEmpty()) {
+            // first parameter is the context, second is the class of the activity to launch
+            Intent i = new Intent(MovieActivity.this, YouTubePlayerActivity.class);
+            // put youTube key into the bundle for access in the youTube activity
+            i.putExtra("youTubeKey", youTubeKey);
+            startActivity(i); // brings up the second activity
         }
-
-        return movie.getYouTubeKey();
     }
 }
